f9fe174be36f2aa1653d04370547d91017c80daf
Gregory Salinas <asalinas720@gmail.com>
2021-03-19 23:35:47 -0700

    Static - Generated Light Mode and Dark Mode Icons

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,9 +2,32 @@ export default {
   target: "static",
   head: {
     titleTemplate: "AGS1130 | %s",
+    link: [
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/light-mode/apple-touch-icon.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/light-mode/favicon-32x32.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/light-mode/favicon-16x16.png",
+      },
+      { rel: "mask-icon", href: "/light-mode/safari-pinned-tab.svg", color: "#000000" },
+      { rel: "manifest", href: "/light-mode/site.webmanifest" },
+    ],
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { name: "msapplication-TileColor", content: "#ffffff" },
+      { name: "theme-color", content: "#ffffff" },
     ],
   },
   components: {
